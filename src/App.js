2eba1780938ee5d5a20900badee6c7437b4c8b78
codeng22 <codeng22@gmail.com>
2023-01-23 21:11:37 +0530

    Initial Project Setup

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var a=15;
+   console.log(a);
+
+   function onsubmit(e,dt){
+    console.log(a);
+    console.log(dt,e);
+   };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={(e)=>{onsubmit("hhhh",e)}}>Submit</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
